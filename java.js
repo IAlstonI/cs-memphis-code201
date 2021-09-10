c916022fcbb63a50ab1f4baee801e0befc3bf677
IAlstonI <85847527+IAlstonI@users.noreply.github.com>
2021-09-10 10:36:23 -0700

    Create java.js

---
+++ java.js
@@ -0,0 +1,23 @@
+ //Problem 1
+ // function numbers() {
+ //     let User = prompt("Pick a number between 0-2.");
+ //     alert("Which number did you pick?");
+
+ //     let User = true;
+ //     while (User) {
+ //         User = prompt("")
+
+
+
+ //     }
+
+ // }
+
+ //Problem 2 
+
+
+ //Problem 3
+ function animals() {
+     let animals = ["Dog", "Cat", "Bird "]
+
+ }
